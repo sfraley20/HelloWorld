fa5618d5af25e4632689e594ebf463534f7a95bb
sfraley20 <s.fraley.05@gmail.com>
2020-08-24 14:43:54 -0400

    my first push

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+//    This is my main declaration. I need these keywords to let java know that this is where
+//    I will put my code(set of instructions).
+    public  static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+
+}
